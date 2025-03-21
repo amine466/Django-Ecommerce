959636011605c87850ae90301af65541cb2bb874
amine466 <aminelitime@rocketmail.com>
2025-03-21 23:15:47 +0100

    New Commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao5(^0fa@nnuv9sft8*!eg@o+mbo58@yn05ml1e!!cjkj_(=5$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
