6b6c4bc70e5aa510099324ec19c22721e911f9e5
amine466 <aminelitime@rocketmail.com>
2025-03-22 15:19:07 +0100

    Last Commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao5(^0fa@nnuv9sft8*!eg@o+mbo58@yn05ml1e!!cjkj_(=5$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-ecommerce-jjqk.onrender.com', '127.0.0.1']
 
@@ -125,9 +125,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 
